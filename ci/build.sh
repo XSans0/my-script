01f5402e6f0e1f4e7e38bfcd9b1c8a9dcf1a104d
XSans0 <xsansdroid@gmail.com>
2022-07-28 17:27:02 +0700

    ci: Switch back to `CROSS_COMPILE_ARM32`
    Back to CROSS_COMPILE_ARM32 for compile my new source.
    
    Signed-off-by: XSans0 <xsansdroid@gmail.com>

--- ci/build.sh
+++ ci/build.sh
@@ -178,7 +178,7 @@ if [[ "${COMPILE}" == "clang" ]]; then
         STRIP=${PrefixDir}llvm-strip \
         CLANG_TRIPLE=${TRIPLE} \
         CROSS_COMPILE=${ARM64} \
-        CROSS_COMPILE_COMPAT=${ARM32} \
+        CROSS_COMPILE_ARM32=${ARM32} \
         LLVM=1 2>&1 | tee ${KERNEL_LOG}
 
     if [[ -f "$KERNEL_IMG" ]]; then
@@ -202,7 +202,7 @@ elif [[ "${COMPILE}" == "gcc" ]]; then
         OBJDUMP=llvm-objdump \
         READELF=llvm-readelf \
         CROSS_COMPILE=${ARM64} \
-        CROSS_COMPILE_COMPAT=${ARM32} 2>&1 | tee ${KERNEL_LOG}
+        CROSS_COMPILE_ARM32=${ARM32} 2>&1 | tee ${KERNEL_LOG}
 
     if [[ -f "$KERNEL_IMG" ]]; then
         END=$(date +"%s")
