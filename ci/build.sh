2dbc63dd36afc1727671627f8f9938050ef67e11
XSans0 <xsansdroid@gmail.com>
2022-08-01 17:49:08 +0700

    ci: Add `CROSS_COMPILE_COMPAT`
    Use CROSS_COMPILE_COMPAT if it doesn't support CROSS_COMPILE_ARM32.
    
    Signed-off-by: XSans0 <xsansdroid@gmail.com>

--- ci/build.sh
+++ ci/build.sh
@@ -181,6 +181,7 @@ if [[ "${COMPILE}" == "clang" ]]; then
         STRIP=${PrefixDir}llvm-strip \
         CLANG_TRIPLE=${TRIPLE} \
         CROSS_COMPILE=${ARM64} \
+        CROSS_COMPILE_COMPAT=${ARM32} \
         CROSS_COMPILE_ARM32=${ARM32} \
         LLVM=1 2>&1 | tee ${KERNEL_LOG}
 
@@ -206,6 +207,7 @@ elif [[ "${COMPILE}" == "gcc" ]]; then
         OBJDUMP=llvm-objdump \
         READELF=llvm-readelf \
         CROSS_COMPILE=${ARM64} \
+        CROSS_COMPILE_COMPAT=${ARM32} \
         CROSS_COMPILE_ARM32=${ARM32} 2>&1 | tee ${KERNEL_LOG}
 
     if [[ -f "$KERNEL_IMG" ]]; then
