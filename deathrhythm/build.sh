944ca60f010f466f4d3d73921dc96df847aa2d5e
XSans0 <xsansdroid@gmail.com>
2023-02-15 14:26:39 +0700

    deathrhythm: Added `LLVM_IAS=1` compilation flag
    Depend on https://github.com/XSans0/kernel_xiaomi_vayu/commit/c9992eff4ae0cf4e072e042c194cbc37b26f52df.
    
    Signed-off-by: XSans0 <xsansdroid@gmail.com>

--- deathrhythm/build.sh
+++ deathrhythm/build.sh
@@ -81,6 +81,7 @@ msg "* Start Compile kernel for $DEVICE using $CPU $CORES thread"
 make O=out "$DEVICE"_defconfig
 make -j"$CORES" O=out \
     LLVM=1 \
+    LLVM_IAS=1 \
     CLANG_TRIPLE="$TRIPLE" \
     CROSS_COMPILE="$ARM64" \
     CROSS_COMPILE_COMPAT="$ARM32" 2>&1 | tee "$KERNEL_LOG"
